2129ec4f2b35cb8f4ee5d4123d306155dfa39acd
Benjamin Romero <programador.ben@gmail.com>
2025-08-21 03:10:42 -0400

    fix: :bug: produccion

--- database/migrations/2025_08_21_061406_prerequisito.php
+++ database/migrations/2025_08_21_061406_prerequisito.php
@@ -8,14 +8,16 @@ return new class extends Migration
 {
     public function up(): void
     {
-        //
+        Schema::create('prerequisitos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('materia_id')->constrained('materias')->onDelete('cascade');
+            $table->foreignId('prerequisito_id')->constrained('materias')->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('prerequisitos');
     }
 };
